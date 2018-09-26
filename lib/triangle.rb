71193cecf4487b6b4e29106c39a1998ded36a86c
kvdoherty <kvdoherty@gmail.com>
2018-09-26 00:33:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  
+attr_accessor :one, :two, :three
+
+def initialize(one, two, three)
+  @one = one 
+  @two = two
+  @three = three
+end
+
+
+def kind
+  if @one == @two && @two == @three 
+    :equilateral
+  elsif (@one == @two && @two != @three) || (@two == @three && @two != @one) || (@one == @three && @one != @two)
+    :isosceles
+  elsif @one != @two && @three != @two
+    :scalene
+  elsif @one == @one  
+    begin
+        raise TriangleError
+       rescue TriangleError => error
+          puts error.message
+      end
+  else 
+      nil
+  end 
+end
+
+class TriangleError < StandardError
+  def message 
+    "you must give the get_married method an argument of an instance of the person class!"
+  end
+end
+
 end
